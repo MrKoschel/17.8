a4b7b241bf12cdfd466701e7cd5fe3746e60cb02
MrKoschel <38735374+MrKoschel@users.noreply.github.com>
2018-12-13 17:26:08 +0100

    Add js

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+process.stdin.setEncoding('utf-8');
+
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+    // tutaj coś się dzieje :)
+});
+
